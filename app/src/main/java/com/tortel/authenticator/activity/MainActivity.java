6ec20e1e4bc2254abf9784f20943ba399d10063c
Scott Warner <Tortel1210@gmail.com>
2019-05-31 14:40:31 -0400

    Remove the wear sync

--- app/src/main/java/com/tortel/authenticator/activity/MainActivity.java
+++ app/src/main/java/com/tortel/authenticator/activity/MainActivity.java
@@ -1,11 +1,9 @@
 package com.tortel.authenticator.activity;
 
-import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,14 +13,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.google.android.gms.tasks.Tasks;
-import com.google.android.gms.wearable.DataClient;
-import com.google.android.gms.wearable.DataItem;
-import com.google.android.gms.wearable.PutDataMapRequest;
-import com.google.android.gms.wearable.Wearable;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.tortel.authenticator.R;
-import com.tortel.authenticator.common.sync.SyncUtils;
 import com.tortel.authenticator.common.data.AccountDb;
 import com.tortel.authenticator.common.utils.DependencyInjector;
 import com.tortel.authenticator.common.utils.Log;
@@ -31,10 +23,6 @@ import com.tortel.authenticator.export.FileImportActivity;
 import com.tortel.authenticator.fragment.AddAccountListDialogFragment;
 import com.tortel.authenticator.fragment.CodeListFragment;
 import com.tortel.authenticator.fragment.NoAccountsFragment;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Main activity that shows the codes and stuff
@@ -91,79 +79,6 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.content_frame, frag).commitAllowingStateLoss();
         }
-    }
-
-    /**
-     * Sync all accounts to wear
-     */
-    @SuppressLint("StaticFieldLeak")
-    private void syncAllAccounts(){
-        final DataClient dataClient = Wearable.getDataClient(this);
-        final List<PutDataMapRequest> requests = new ArrayList<>();
-        for(Integer id : mAccountDb.getAllIds()) {
-            requests.add(SyncUtils.createDataMap(id, mAccountDb));
-        }
-
-        (new AsyncTask<Void, Void, Void>(){
-            @Override
-            protected Void doInBackground(Void... voids) {
-                try {
-                    for (PutDataMapRequest req : requests) {
-                        DataItem item = Tasks.await(dataClient.putDataItem(req.asPutDataRequest()));
-                        Log.d("Sent " + item.toString());
-                    }
-                } catch (InterruptedException | ExecutionException e) {
-                    Log.e("Exception deleting wear data", e);
-                }
-                return null;
-            }
-        }).execute();
-    }
-
-    /**
-     * Sync a specific account to wear
-     * @param id
-     */
-    @SuppressLint("StaticFieldLeak")
-    private void syncAccount(final int id){
-        final DataClient dataClient = Wearable.getDataClient(this);
-        final PutDataMapRequest req = SyncUtils.createDataMap(id, mAccountDb);
-
-        (new AsyncTask<Void, Void, Void>(){
-            @Override
-            protected Void doInBackground(Void... voids) {
-                try {
-                    DataItem item = Tasks.await(dataClient.putDataItem(req.asPutDataRequest()));
-                    Log.d("Sent " + item.toString());
-                } catch (InterruptedException | ExecutionException e) {
-                    Log.e("Exception deleting wear data", e);
-                }
-                return null;
-            }
-        }).execute();
-    }
-
-    /**
-     * Delete a specific account from wear
-     * @param id
-     */
-    @SuppressLint("StaticFieldLeak")
-    private void deleteAccount(final int id){
-        final DataClient dataClient = Wearable.getDataClient(this);
-        final PutDataMapRequest req = SyncUtils.createDataMap(id, mAccountDb);
-
-        (new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... voids) {
-                try {
-                    int result = Tasks.await(dataClient.deleteDataItems(req.getUri()));
-                    Log.d("Deleted " + req.getUri() + " with result " + result);
-                } catch (InterruptedException | ExecutionException e) {
-                    Log.e("Exception deleting wear data", e);
-                }
-                return null;
-            }
-        }).execute();
     }
 
     @Override
@@ -247,21 +162,12 @@ public class MainActivity extends AppCompatActivity {
             switch(intent.getAction()){
                 case ACCOUNT_CREATED:
                     showFragment();
-                    syncAllAccounts();
                     return;
                 case ACCOUNT_DELETED:
-                    // TODO - Propigate delete to wear
-                    id = intent.getIntExtra(ACCOUNT_ID, -1);
-                    if(id > 0){
-                        syncAccount(id);
-                    }
                     showFragment();
                     return;
                 case ACCOUNT_CHANGED:
                     id = intent.getIntExtra(ACCOUNT_ID, -1);
-                    if(id > 0){
-                        syncAccount(id);
-                    }
                     return;
             }
         }
